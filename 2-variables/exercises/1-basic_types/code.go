b3856debc3ae188f541a62b212d785b1efc5cdcb
Schubert Rodriguez <schubert.david.r@gmail.com>
2025-04-12 22:21:09 +0100

    feat: Initialize variables for SMS sending limit, cost, permission, and username in basic types exercise

--- 2-variables/exercises/1-basic_types/code.go
+++ 2-variables/exercises/1-basic_types/code.go
@@ -5,5 +5,10 @@ import "fmt"
 func main() {
 	// initialize variables here
 
+	var smsSendingLimit int = 0
+	var costPerSMS float64 = 0.0
+	var hasPermission bool = true
+	var username string = "wagslane"
+
 	fmt.Printf("%v %f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
 }
